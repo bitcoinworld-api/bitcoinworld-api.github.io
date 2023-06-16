f47ba633747f60b4972e443fb8063cfedce2ec76
bitcoinworld-api <136670715+bitcoinworld-api@users.noreply.github.com>
2023-06-16 18:15:31 +0800

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -11,6 +11,5 @@ REMOTE=$(git log refs/remotes/origin/$BRANCH -n 1 --pretty=format:"%H")
 if [ $LOCAL = $REMOTE ]; then
     echo "Not update, finish"
 else
-    echo "Need update, run deploy.sh"
-    ./deploy.sh
+    sh ./deploy.sh
 fi

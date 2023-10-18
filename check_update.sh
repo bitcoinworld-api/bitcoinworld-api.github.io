7cd13168d42cd0e2f6891fca164dd076ce632826
bitcoinworld-api <136670715+bitcoinworld-api@users.noreply.github.com>
2023-10-18 17:14:06 +0800

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -6,7 +6,8 @@ BRANCH=v1_cn
 
 LOCAL=$(git log $BRANCH -n 1 --pretty=format:"%H")
 
-git pull origin $BRANCH
+# git pull origin $BRANCH
+git pull
 
 REMOTE=$(git log refs/remotes/origin/$BRANCH -n 1 --pretty=format:"%H")
 
